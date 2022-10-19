4532deadfebbfbe19f9f1f7d5bcd8b7574bc9c09
vaguilar07 <113448472+vaguilar07@users.noreply.github.com>
2022-10-18 20:51:17 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.x < scene.screenWidth() / 2) {
+        info.changeScoreBy(1)
+    }
+    if (mySprite.y < scene.screenWidth() / 2) {
+        info.changeLifeBy(-1)
+    }
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+info.setLife(5)
